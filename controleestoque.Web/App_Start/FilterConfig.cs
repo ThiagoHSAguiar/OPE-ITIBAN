a0b4c31537f0a85e6c47eec2dd10b4d153a07a76
Thiago Aguiar <aguiariuga@hotmail.com>
2019-06-29 09:55:55 -0300

    Configuração projeto

---
+++ controleestoque.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace controleestoque.Web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ controleestoque.Web/Models/UsuarioModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+
+namespace controleestoque.Web.Models
+{
+    public class UsuarioModel
+    {
+        public static bool ValidarUsuario(string login, string senha)
+        {
+            var ret = false;
+
+            using (var conexao = new SqlConnection())
+            {
+                conexao.ConnectionString = ConfigurationManager.ConnectionStrings["principal"].ConnectionString;
+                conexao.Open();
+                using (var comando = new SqlCommand())
+                {
+                    string criptSenha = CriptoHelper.HashMD5(senha);
+                    comando.Connection = conexao;
+                    comando.CommandText = string.Format(
+                        "select count(*) from usuario where login = '{0}' and senha='{1}'",
+                        login, criptSenha);
+                    ret = ((int)comando.ExecuteScalar() > 0);
+                }
+            }
+            return ret;
+
+        }
+    }
+}
